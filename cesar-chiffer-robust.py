0a5f45437211dca59e73196d95b70b47e3cffee6
Lisa12344321 <lisa.alfven@elev.ga.ntig.se>
2026-02-24 13:25:09 +0100

    funkar nu

--- cesar-chiffer-robust.py
+++ cesar-chiffer-robust.py
@@ -1,21 +1,24 @@
 alfabet = "abcdefghijklmnopqrstuvwxyzåäö"
 
-meddelande = "Hej, vad gör du idag!?"
+meddelande = "Hej, vad GÖr du iDag!?"
 
-nyckel = 1
+nyckel = 700
 
-resultat = meddelande
+resultat = ""
 
 for bokstav in meddelande:
     try:
         if bokstav.isalpha():
-            plats = (alfabet.index(bokstav.lower()) + nyckel) % 29
-            if bokstav.isupper():  
-                resultat = resultat.replace(bokstav, alfabet[plats].upper())
+            b = alfabet[(alfabet.index(bokstav.lower()) + nyckel) % 29]
+            if bokstav.isupper():
+                b = b.upper()
+                resultat += b
             else:
-                resultat = resultat.replace(bokstav, alfabet[plats])
-        
-                print(resultat)
+                resultat += b
+
+            print(resultat)
+        else:
+            resultat += bokstav
     except:
         None
 
